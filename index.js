d98500db475c66314054c76c23880520fd00b171
MAwais <mawais54013@outlook.com>
2019-12-11 20:58:36 -0800

    added info

---
+++ index.js
@@ -0,0 +1,6 @@
+function allPlanets() {
+    const planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    return planets;
+}
+
+module.exports.getPlanets = allPlanets;
